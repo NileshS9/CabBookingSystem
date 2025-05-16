e74bbb274b0a1f684a9f6838b5355488cfb57756
NileshS9 <sawantnilesh891@gmail.com>
2025-05-16 23:16:38 +0530

    Created Driver entity in Model

---
+++ GoCab/src/main/java/Model/Driver.java
@@ -0,0 +1,26 @@
+package Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String currentLocation;
+
+    private boolean available = true;
+}
