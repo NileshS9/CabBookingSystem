4985d174df18fec85eb4c35f238b5cc10879888b
NileshS9 <sawantnilesh891@gmail.com>
2025-05-16 22:19:23 +0530

    Day 1 One Done

---
+++ GoCab/src/main/java/com/Book/GoCab/GoCabApplication.java
@@ -0,0 +1,13 @@
+package com.Book.GoCab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoCabApplication.class, args);
+	}
+
+}
---
+++ GoCab/src/test/java/com/Book/GoCab/GoCabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Book.GoCab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoCabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
