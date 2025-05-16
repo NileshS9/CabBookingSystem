b79eedd3ab18be4d0ddcb80a73e824811eed62fc
NileshS9 <sawantnilesh891@gmail.com>
2025-05-16 23:42:43 +0530

    Created Ride entity in Model

---
+++ GoCab/src/main/java/com/Book/GoCab/Model/Ride.java
@@ -0,0 +1,31 @@
+package com.Book.GoCab.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ride {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Driver driver;
+
+    private String pickUpLocation;
+
+    private String dropLocation;
+
+    private double fare;
+
+    private String status; //Booked ,In Progress ,Completed
+}
