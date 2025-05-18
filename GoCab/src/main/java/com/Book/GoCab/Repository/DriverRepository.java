1e304c32b640a87ea5290656f34409df05cd185e
NileshS9 <sawantnilesh891@gmail.com>
2025-05-18 13:47:56 +0530

    Created a Driverrepository interface file in Repo dir

---
+++ GoCab/src/main/java/com/Book/GoCab/Repository/DriverRepository.java
@@ -0,0 +1,10 @@
+package com.Book.GoCab.Repository;
+
+import com.Book.GoCab.Model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DriverRepository extends JpaRepository<Driver,Long> {
+    List<Driver> findByAvailableTrue();
+}
