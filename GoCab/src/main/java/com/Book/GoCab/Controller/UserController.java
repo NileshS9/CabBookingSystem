7f1290f1bf8bd552e4b05dc3b858a15a2a3ab498
NileshS9 <sawantnilesh891@gmail.com>
2025-05-21 22:35:51 +0530

    Created UserControlller in Controller dir

---
+++ GoCab/src/main/java/com/Book/GoCab/Controller/UserController.java
@@ -0,0 +1,25 @@
+package com.Book.GoCab.Controller;
+
+import com.Book.GoCab.Model.User;
+import com.Book.GoCab.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User savedUser = userRepository.save(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+}
